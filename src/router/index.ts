b8d0e256ac4cfa531976c0e7c1b1cc2ccb5c94ff
PRHyzzza <1875377706@qq.comgit config --global user.name PRHyzzza git config --global user.email 1875377706@qq.com>
2022-07-24 17:37:06 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import NProgress from 'nprogress'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('../pages/index.vue'),
+
+  },
+  {
+    path: '/:pathMatch(.*)',
+    redirect: '/404'
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach((to, from) => {
+  NProgress.done()
+})
+
+export default router
