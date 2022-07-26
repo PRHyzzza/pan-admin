037c168b33f43446e6fa4172a64941442be0bf90
prh <1875377706@qq.com>
2022-07-26 15:50:10 +0800

    aside

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,31 +6,43 @@ const handleOpen = (key: string, keyPath: string[]) => {
 const handleClose = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
+const collapse = computed(() => {
+  return isCollapse.value ? '64px' : '200px'
+})
 </script>
 
 <template>
   <div h100vh w100vw flex overflow-hidden>
     <el-container>
-      <el-aside :width="!isCollapse ? '200px' : '64px'" bg-gray-700 text-center>
-        <div py-4 text-white flex flex-row items-center justify-center gap-2>
-          <div i-carbon:gui-management></div>
-          <div v-show="!isCollapse">后台管理</div>
+      <el-aside :width="collapse" bg-gray-700 text-center>
+        <div class="sidebar-logo-container" flex items-center justify-center text-white py-18px>
+          <transition name="sidebarLogoFade">
+            <router-link v-if="isCollapse" key="collapse" to="/">
+              <div i-carbon:gui-management text-white></div>
+            </router-link>
+            <router-link v-else key="expand" to="/" flex flex-row>
+              <div i-carbon:gui-management mr-12px text-5></div>
+              <h1 class="sidebar-title">后台管理</h1>
+            </router-link>
+          </transition>
         </div>
-        <el-menu default-active="1" :collapse="isCollapse" active-text-color="#4169e1" background-color="#374151"
-          text-color="#fff" @open="handleOpen" @close="handleClose">
-          <el-menu-item index="1">
-            <el-icon>
-              <document />
-            </el-icon>
-            <template #title>Navigator 1</template>
-          </el-menu-item>
-          <el-menu-item index="2">
-            <el-icon>
-              <setting />
-            </el-icon>
-            <template #title>Navigator 2</template>
-          </el-menu-item>
-        </el-menu>
+        <el-scrollbar>
+          <el-menu default-active="1" :collapse="isCollapse" active-text-color="#4169e1" background-color="#374151"
+            text-color="#fff" @open="handleOpen" @close="handleClose">
+            <el-menu-item index="1">
+              <el-icon>
+                <document />
+              </el-icon>
+              <template #title>Navigator 1</template>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <el-icon>
+                <setting />
+              </el-icon>
+              <template #title>Navigator 2</template>
+            </el-menu-item>
+          </el-menu>
+        </el-scrollbar>
       </el-aside>
       <el-container>
         <el-header bg-blue flex flex-row gap-4px items-center>
@@ -53,5 +65,14 @@ const handleClose = (key: string, keyPath: string[]) => {
   overflow: hidden;
   transition: ease-in-out 0.3s;
   transition-timing-function: ease-in-out;
+}
+
+.sidebarLogoFade-enter-active {
+  transition: opacity 1.5s;
+}
+
+.sidebarLogoFade-enter,
+.sidebarLogoFade-leave-to {
+  opacity: 0;
 }
 </style>
